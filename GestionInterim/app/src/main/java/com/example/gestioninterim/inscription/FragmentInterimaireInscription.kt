a63e53d27301042b9f3da6408c5fc3f14d165be9
Reyne Yoann <yoann.reyne@etu.umontpellier.fr>
2023-04-09 23:18:42 +0200

    add

--- GestionInterim/app/src/main/java/com/example/gestioninterim/inscription/FragmentInterimaireInscription.kt
+++ GestionInterim/app/src/main/java/com/example/gestioninterim/inscription/FragmentInterimaireInscription.kt
@@ -1,18 +1,83 @@
 package com.example.gestioninterim.inscription
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.CheckBox
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.gestioninterim.R
+import com.google.android.material.textfield.TextInputLayout
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.provider.OpenableColumns
+import android.widget.Toast
 
 class FragmentInterimaireInscription : Fragment() {
+
+    val myLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+
+            val selectedFileUri = result.data?.data
+
+            println(selectedFileUri)
+
+        }
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
 
         val view = inflater.inflate(R.layout.fragment_inscription_interimaire, container, false)
+
+        // Récupération du bouton pour selectionner le cv
+        val getCv = view.findViewById<TextInputLayout>(R.id.inscriptionInterimaireCv)
+
+        // Adapter pour la sélection des pays
+        val nationalities = resources.getStringArray(R.array.nationalities)
+        val adapter = ArrayAdapter<String>(requireContext(), android.R.layout.simple_list_item_1, nationalities)
+        val autoCompleteTextView = view.findViewById<AutoCompleteTextView>(R.id.nationality_dropdown)
+        autoCompleteTextView.setAdapter(adapter)
+
+        // Listener pour le bouton CV
+        val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+                isGranted: Boolean ->
+
+                if (isGranted) {
+                    val intent = Intent(Intent.ACTION_GET_CONTENT)
+                    intent.type = "*/*"
+                    myLauncher.launch(intent)
+                } else {
+                    // Explain to the user that the feature is unavailable because the
+                    // feature requires a permission that the user has denied. At the
+                    // same time, respect the user's decision. Don't link to system
+                    // settings in an effort to convince the user to change their
+                    // decision.
+                }
+            }
+
+
+        getCv.setEndIconOnClickListener{
+            when {
+                ContextCompat.checkSelfPermission(requireContext(),Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED -> {
+                    val intent = Intent(Intent.ACTION_GET_CONTENT)
+                    intent.type = "*/*"
+                    myLauncher.launch(intent)
+                }
+                shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE) -> {
+                }
+                else -> {
+                    println("Je suis là 3")
+                    requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+                }
+            }
+        }
 
         return view
     }
